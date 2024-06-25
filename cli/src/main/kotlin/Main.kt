1a50d4fcbe4264dc81b5cefcac34200d8c79b52e
Daniel van Dorp <daniel.van.dorp@mendix.com>
2024-06-25 19:33:27 +0200

    DO: fetch latest version info instead of hardcoding it
    retrieveBinaryFileInfo still requires imeiSerial later.

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -2,20 +2,48 @@ package tk.zwander
 
 import kotlinx.coroutines.runBlocking
 import tk.zwander.common.tools.Request
+import tk.zwander.common.tools.VersionFetch
 
 fun main() {
+    val model: String = "SM-T290"
+    val region: String = "PHN"
+    val imeiSerial: String = "R9WMC1ZDYZJ"
+
     println("Starting...")
     runBlocking {
-        val info = Request.retrieveBinaryFileInfo(
-            fw = "T290XXS5CWG5/T290OXM5CWG5/T290XXS5CWG5/T290XXS5CWG5",
-            model = "SM-T290",
-            region = "PHN",
-            imeiSerial = "R9WMC1ZDYZJ",
+        val fw = getLatestVersionInfo(model, region)
+        println(fw)
+
+        val fileInfo = Request.retrieveBinaryFileInfo(
+            fw = fw, // "T290XXS5CWG5/T290OXM5CWG5/T290XXS5CWG5/T290XXS5CWG5"
+            model = model,
+            region = region,
+            imeiSerial = imeiSerial,
             onFinish = {
                 println(it)
             },
             onVersionException = null
         )
-        println(info)
+        println(fileInfo)
     }
+}
+
+fun getLatestVersionInfo(model: String, region: String): String {
+    runBlocking {
+        val (fw, os, error, output) = VersionFetch.getLatestVersion(
+            model = model,
+            region = region,
+        )
+
+        if (error == null) {
+            //println(output.replace("\t", "  "))
+            println(fw)
+            //println(os)
+            return@runBlocking fw
+        } else {
+            println(error.message.toString())
+            return@runBlocking ""
+        }
+    }
+    return ""
 }
