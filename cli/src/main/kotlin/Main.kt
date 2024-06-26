6d4f049b5422436c83bae338ef4de914b4fca00c
Daniel van Dorp <daniel.van.dorp@mendix.com>
2024-06-26 14:10:57 +0200

    Revert "DO: an even cleaner solution"
    This reverts commit f1cef6f8fcb7834eb4a39bb2dbda6b455df7fc32.

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -4,35 +4,31 @@ import kotlinx.coroutines.runBlocking
 import tk.zwander.common.tools.Request
 import tk.zwander.common.tools.VersionFetch
 
-fun main(): Unit = runBlocking {
-    val model = "SM-T290"
-    val region = "PHN"
-    val imeiSerial = "R9WMC1ZDYZJ"
+fun main() {
+    val model: String = "SM-T290"
+    val region: String = "PHN"
+    val imeiSerial: String = "R9WMC1ZDYZJ"
 
     println("Starting...")
+    runBlocking {
+        val fw = getLatestVersionInfo(model, region)
+        println(fw)
 
-    getLatestVersionInfo(model, region)
-        .onSuccess { fw ->
-            println("Got latest version info: $fw")
-            Request.retrieveBinaryFileInfo(
-                fw = fw,
-                model = model,
-                region = region,
-                imeiSerial = imeiSerial,
-                onFinish = {
-                    println(it)
-                },
-                onVersionException = null
-            ).also {
+        val fileInfo = Request.retrieveBinaryFileInfo(
+            fw = fw, // "T290XXS5CWG5/T290OXM5CWG5/T290XXS5CWG5/T290XXS5CWG5"
+            model = model,
+            region = region,
+            imeiSerial = imeiSerial,
+            onFinish = {
                 println(it)
-            }
-        }
-        .onFailure {
-            println(it)
-        }
+            },
+            onVersionException = null
+        )
+        println(fileInfo)
+    }
 }
 
-suspend fun getLatestVersionInfo(model: String, region: String): Result<String> {
+suspend fun getLatestVersionInfo(model: String, region: String): String {
     val (fw, os, error, output) = VersionFetch.getLatestVersion(
         model = model,
         region = region,
@@ -40,9 +36,11 @@ suspend fun getLatestVersionInfo(model: String, region: String): Result<String> 
 
     if (error == null) {
         //println(output.replace("\t", "  "))
-        //println(fw)
+        println(fw)
         //println(os)
-        return Result.success(fw)
+        return fw
+    } else {
+        println(error.message.toString())
+        return ""
     }
-    return Result.failure(Exception(error.message.toString()))
 }
