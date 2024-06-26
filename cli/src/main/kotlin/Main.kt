f1cef6f8fcb7834eb4a39bb2dbda6b455df7fc32
Daniel van Dorp <daniel.van.dorp@mendix.com>
2024-06-26 13:48:07 +0200

    DO: an even cleaner solution

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -4,31 +4,35 @@ import kotlinx.coroutines.runBlocking
 import tk.zwander.common.tools.Request
 import tk.zwander.common.tools.VersionFetch
 
-fun main() {
-    val model: String = "SM-T290"
-    val region: String = "PHN"
-    val imeiSerial: String = "R9WMC1ZDYZJ"
+fun main(): Unit = runBlocking {
+    val model = "SM-T290"
+    val region = "PHN"
+    val imeiSerial = "R9WMC1ZDYZJ"
 
     println("Starting...")
-    runBlocking {
-        val fw = getLatestVersionInfo(model, region)
-        println(fw)
 
-        val fileInfo = Request.retrieveBinaryFileInfo(
-            fw = fw, // "T290XXS5CWG5/T290OXM5CWG5/T290XXS5CWG5/T290XXS5CWG5"
-            model = model,
-            region = region,
-            imeiSerial = imeiSerial,
-            onFinish = {
+    getLatestVersionInfo(model, region)
+        .onSuccess { fw ->
+            println("Got latest version info: $fw")
+            Request.retrieveBinaryFileInfo(
+                fw = fw,
+                model = model,
+                region = region,
+                imeiSerial = imeiSerial,
+                onFinish = {
+                    println(it)
+                },
+                onVersionException = null
+            ).also {
                 println(it)
-            },
-            onVersionException = null
-        )
-        println(fileInfo)
-    }
+            }
+        }
+        .onFailure {
+            println(it)
+        }
 }
 
-suspend fun getLatestVersionInfo(model: String, region: String): String {
+suspend fun getLatestVersionInfo(model: String, region: String): Result<String> {
     val (fw, os, error, output) = VersionFetch.getLatestVersion(
         model = model,
         region = region,
@@ -36,11 +40,9 @@ suspend fun getLatestVersionInfo(model: String, region: String): String {
 
     if (error == null) {
         //println(output.replace("\t", "  "))
-        println(fw)
+        //println(fw)
         //println(os)
-        return fw
-    } else {
-        println(error.message.toString())
-        return ""
+        return Result.success(fw)
     }
+    return Result.failure(Exception(error.message.toString()))
 }
